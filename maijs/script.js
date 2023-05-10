ec0b289451fa05e2e48c7d07cbae0d71e6d4199f
leeldee <114051191+leeldee@users.noreply.github.com>
2023-05-10 09:35:44 +0300

    Add files via upload

---
+++ maijs/script.js
@@ -0,0 +1,7 @@
+console.log(123)
+const coolButton = document.querySelector('.js-cool')
+console.log(coolButton)
+
+coolButton.addEventListener('click', ()=> {
+    window.open('urlhttps://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
